a0025b99d3204f5f194d81e2b5c4e5e8d3eab711
Mikael Kermorgant <mikael.kermorgant@gmail.com>
2016-09-27 12:24:11 +0300

    proof of concept
    slideshow module draft.
    -> add/edit form, very basic : you have to add mediaIds between brackets
    in the input field, like this [19],[22]
    ->getResource generates a basic slideshow, which at least works in
    preview mode (only one effect for now)

--- custom/Slideshow.php
+++ custom/Slideshow.php
@@ -19,7 +19,7 @@
  * along with Xibo.  If not, see <http://www.gnu.org/licenses/>.
  *
  */
-namespace Xibo\Widget;
+namespace Xibo\Custom;
 
 use GuzzleHttp\Client;
 use GuzzleHttp\Exception\RequestException;
@@ -27,6 +27,7 @@ use Respect\Validation\Validator as v;
 use Xibo\Entity\Media;
 use Xibo\Exception\NotFoundException;
 use Xibo\Factory\ModuleFactory;
+use Xibo\Widget\ModuleWidget;
 
 class Slideshow extends ModuleWidget
 {
@@ -79,61 +80,15 @@ class Slideshow extends ModuleWidget
     {
         $this->mediaFactory->createModuleSystemFile(PROJECT_ROOT . '/web/modules/jquery-cycle-2.1.6.min.js')->save();
         $this->mediaFactory->createModuleSystemFile(PROJECT_ROOT . '/web/modules/xibo-layout-scaler.js')->save();
-
-        foreach ($this->mediaFactory->createModuleFileFromFolder($this->resourceFolder) as $media) {
-            /* @var Media $media */
-            $media->save();
-        }
     }
 
     /**
      * Form for updating the module settings
      */
-    public function settingsForm()
-    {
-        return 'slideshow-form-settings';
-    }
-
-    /**
-     * Process any module settings
-     */
-    // public function settings()
+    // public function settingsForm()
     // {
-    //     // Process any module settings you asked for.
-    //     $apiKey = $this->getSanitizer()->getString('apiKey');
-
-    //     if ($apiKey == '')
-    //         throw new \InvalidArgumentException(__('Missing API Key'));
-
-    //     $this->module->settings['apiKey'] = $apiKey;
-    //     $this->module->settings['cachePeriod'] = $this->getSanitizer()->getInt('cachePeriod', 300);
+    //     return 'slideshow-form-settings';
     // }
-
-    /**
-     * Loads templates for this module
-     */
-    private function loadTemplates()
-    {
-        // Scan the folder for template files
-        foreach (glob(PROJECT_ROOT . '/modules/forecastio/*.template.json') as $template) {
-            // Read the contents, json_decode and add to the array
-            $this->module->settings['templates'][] = json_decode(file_get_contents($template), true);
-        }
-
-        $this->getLog()->debug(count($this->module->settings['templates']));
-    }
-
-    /**
-     * Templates available
-     * @return array
-     */
-    public function templatesAvailable()
-    {
-        if (!isset($this->module->settings['templates']))
-            $this->loadTemplates();
-
-        return $this->module->settings['templates'];
-    }
 
     public function validate()
     {
@@ -150,6 +105,9 @@ class Slideshow extends ModuleWidget
         $this->setDuration($this->getSanitizer()->getInt('duration', $this->getDuration()));
         $this->setUseDuration($this->getSanitizer()->getCheckbox('useDuration'));
 
+        $this->setOption('mediaList', $this->getSanitizer()->getString('mediaList'));
+        
+        
         // Save the widget
         $this->validate();
         $this->saveWidget();
@@ -162,15 +120,12 @@ class Slideshow extends ModuleWidget
     {
         $this->setDuration($this->getSanitizer()->getInt('duration', $this->getDuration()));
         $this->setUseDuration($this->getSanitizer()->getCheckbox('useDuration'));
-
+        $this->setOption('mediaList', $this->getSanitizer()->getString('mediaList'));
+        
         // Save the widget
         $this->validate();
         $this->saveWidget();
     }
-
-
-
-
 
 
 
@@ -181,110 +136,57 @@ class Slideshow extends ModuleWidget
      */
     public function getResource($displayId = 0)
     {
-        // Behave exactly like the client.
-        if (!$foreCast = $this->getForecastData($displayId))
-            return '';
-
-        // Do we need to override the language?
-        // TODO: I don't like this date fix, the library should really check the file exists?
-        if ($this->getOption('lang', 'en') != 'en' && file_exists(PROJECT_ROOT . '/vendor/jenssegers/date/src/Lang/' . $this->getOption('lang') . '.php')) {
-            $this->getDate()->setLocale($this->getOption('lang'));
-        }
-
         $data = [];
         $isPreview = ($this->getSanitizer()->getCheckbox('preview') == 1);
 
+        // Clear all linked media.
+        $this->clearMedia();
+
         // Replace the View Port Width?
         $data['viewPortWidth'] = ($isPreview) ? $this->region->width : '[[ViewPortWidth]]';
-
-        $headContent = '
-            <link href="' . $this->getResourceUrl('forecastio/weather-icons.min.css') . '" rel="stylesheet" media="screen">
-            <style type="text/css">
-                .container { color: ' . $this->getOption('color', '000') . '; }
-                ' . $this->parseLibraryReferences($isPreview, $this->getRawNode('styleSheet', null)) . '
-            </style>
-        ';
-
-        // Add our fonts.css file
-        $headContent .= '<link href="' . (($isPreview) ? $this->getApp()->urlFor('library.font.css') : 'fonts.css') . '" rel="stylesheet" media="screen">';
-        $headContent .= '<style type="text/css">' . file_get_contents($this->getConfig()->uri('css/client.css', true)) . '</style>';
-
-        // Replace any icon sets
-        $data['head'] = str_replace('[[ICONS]]', $this->getResourceUrl('forecastio/' . $this->getOption('icons')), $headContent);
-
-        // Make some body content
-        $body = $this->parseLibraryReferences($isPreview, $this->getRawNode('currentTemplate', null));
-        $dailyTemplate = $this->parseLibraryReferences($isPreview, $this->getRawNode('dailyTemplate', null));
-
-        // Get the JavaScript node
-        $javaScript = $this->parseLibraryReferences($isPreview, $this->getRawNode('javaScript', ''));
-
-        // Do we need to scale the inner content? Size provided?
-        $size = $this->getOption('size', 1);
-
-        if ($size != 1) {
-            $javaScript .= '
-                <script type="text/javascript">
-                    if ($("body").hasClass("ie7") || $("body").hasClass("ie8")) {
-                        $("#content").css({
-                            "filter": "progid:DXImageTransform.Microsoft.Matrix(M11=' . $size . ', M12=0, M21=0, M22=' . $size . ', SizingMethod=\'auto expand\'"
-                        });
-                    }
-                    else {
-                        $("#content").css({
-                            "transform": "scale(' . $size . ')",
-                            "transform-origin": "top center"
-                        });
-                    }
-                </script>
-            ';
-        }
-
-        // Handle the daily template (if its here)
-        if (stripos($body, '[dailyForecast]')) {
-            // Pull it out, and run substitute over it for each day
-            $dailySubs = '';
-            // Substitute for every day (i.e. 7 times).
-            for ($i = 0; $i < 7; $i++) {
-                $dailySubs .= $this->makeSubstitutions($foreCast['daily']['data'][$i], $dailyTemplate);
-            }
-
-            // Substitute the completed template
-            $body = str_replace('[dailyForecast]', $dailySubs, $body);
-        }
-
-        // Run replace over the main template
-        $data['body'] = $this->makeSubstitutions($foreCast['currently'], $body);
-
-
-        // JavaScript to control the size (override the original width and height so that the widget gets blown up )
-        $options = array(
-            'previewWidth' => $this->getSanitizer()->getDouble('width', 0),
-            'previewHeight' => $this->getSanitizer()->getDouble('height', 0),
-            'originalWidth' => $this->region->width,
-            'originalHeight' => $this->region->height,
-            'scaleOverride' => $this->getSanitizer()->getDouble('scale_override', 0)
-        );
-
+        
+        $duration = $this->getCalculatedDurationForGetResource();
+        
+        $mediaList = explode(',',$this->parseLibraryReferences($isPreview, $this->getOption('mediaList', null)));
+        
+        // Replace the head content
         $javaScriptContent = '<script type="text/javascript" src="' . $this->getResourceUrl('vendor/jquery-1.11.1.min.js') . '"></script>';
+        $javaScriptContent .= '<script type="text/javascript" src="' . $this->getResourceUrl('vendor/jquery-cycle-2.1.6.min.js') . '"></script>';
         $javaScriptContent .= '<script type="text/javascript" src="' . $this->getResourceUrl('xibo-layout-scaler.js') . '"></script>';
-        $javaScriptContent .= '<script>
 
-            var options = ' . json_encode($options) . '
+        $javaScriptContent .=  <<<'EOD'
+<script type="text/javascript">
+				      $('#slide').cycle({ 
+							fx:      'tileSlide', 
+							delay:   100,
+							fit: 1
+							});
+</script>
+EOD;
 
-            $(document).ready(function() {
-                $("body").xiboLayoutScaler(options);
-            });
-        </script>';
-        $javaScriptContent .= $javaScript;
+                           
 
-        // Replace the After body Content
-        $data['javaScript'] = $javaScriptContent;
-
+        
         // Update and save widget if we've changed our assignments.
         if ($this->hasMediaChanged())
-            $this->widget->save(['saveWidgetOptions' => false, 'notifyDisplays' => true]);
+            $this->widget->save(['saveWidgetOptions' => false, 'notifyDisplays' => true]);        
 
+        $i = 0;
+        $body = '<h1>slideshow</h1>';
+        foreach($mediaList as $media) {
+            $i++;
+            if ($i == 1) {
+                $body .= '<div id="slide">';
+            }
+            $body .= '<img src="' . $media . '"/>';
+        }
+        if ($i > 0) {
+            $body .= '</div>';
+        }
+        
+        $data['javaScript'] = $javaScriptContent;
+        $data['body'] = $body;
+        
         // Return that content.
         return $this->renderTemplate($data);
     }
